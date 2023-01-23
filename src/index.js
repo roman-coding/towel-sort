77a4276198415ffa2318bc1a321670fd5dfffe4e
roman-coding <mbizgle@gmail.com>
2023-01-23 17:31:55 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix || matrix.length === 0) return [];
+
+    const res = matrix.reduce((acc, cur, index) => {
+        return acc.concat(index % 2 === 0 ? cur : cur.reverse());
+    }, []);
+
+    return res;
+};
